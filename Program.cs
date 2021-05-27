b90c02e3422e9ac761caa4885de293337c485906
Hemanth T R <trhemanth2@gmail.com>
2021-05-27 09:14:39 +0530

    init enumeration example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
